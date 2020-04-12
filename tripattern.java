c68029bb2145808a0bd5dcc428d9ead2dd7e45d7
Rutujatupkar1996 <63243820+Rutujatupkar1996@users.noreply.github.com>
2020-04-12 22:54:02 +0530

    Update tripattern.java

--- tripattern.java
+++ tripattern.java
@@ -1,19 +1,19 @@
 package rutuja;
 
-public class tripattern {
+public class TriPattern {
 	public static void main(String[] args)
 	{
-		for(int i=1;i<=5;i++)
+		for(int i=1;i<=5;i++)// outer loop for number of rows
 		{
-			for(int j=5;j>=i;j--)
+			for(int j=5;j>=i;j--)// inner loop for number of columns
 			{
-				System.out.print(" ");
+				System.out.print(" ");// print space
 			}
-	for(int k=1;k<=i;k++)
-	{
-		System.out.print(" *");
-	}
-	System.out.println();
+	                   for(int k=1;k<=i;k++)// used for printing star
+	           {
+		System.out.print(" *");// print star
+	           }
+	      System.out.println();//ending line after each row
 		}
 	}
 	
