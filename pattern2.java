f77fdba9be9663f67d2bebeb780dad5ae95da34d
Rutujatupkar1996 <63243820+Rutujatupkar1996@users.noreply.github.com>
2020-04-12 23:12:31 +0530

    Update pattern2.java

--- pattern2.java
+++ pattern2.java
@@ -1,24 +1,26 @@
 package rutuja;
 
-public class pattern2 {
+public class Pattern2 {
 	
 	public static void main(String[] args)
 	{
-		for(int i=1;i<=5;i++)
+		// Printing upper half of the pattern 
+		for(int i=1;i<=5;i++) // outer loop for number of rows
 		{
-			for(int j=1;j<=i;j++)
+			for(int j=1;j<=i;j++) // inner loop for number of columns
 			{
-				System.out.print("*");
+				System.out.print("*"); // print star
 			}
-			System.out.println();
+			System.out.println(); //ending line after each row
 		}
-		for(int i=1;i<=5;i++)
+		// Printing lower half of pattern
+		for(int i=1;i<=5;i++) // inner loop for rows
 		{
-			for(int j=4;j>=i;j--)
+			for(int j=4;j>=i;j--) // outer loop for columns
 			{
-				System.out.print("*");
+				System.out.print("*"); // print star
 			}
-			System.out.println();
+			System.out.println(); //ending line after each rows
 	}
 
 }
